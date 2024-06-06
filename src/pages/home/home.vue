402a6610d12ec41c2107fd5cd57939c173dede7c
zhengxiao <zhengxiao0205@outlook.com>
2024-06-06 12:03:20 +0800

    修改readme

---
+++ src/pages/home/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <view>
+    <web-view src="/static/hybrid/index.html"></web-view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  onLoad() {},
+  methods: {
+    rightClick() {
+      console.log('rightClick')
+    },
+    leftClick() {
+      console.log('leftClick')
+    },
+  },
+}
+</script>
+
+<style></style>
